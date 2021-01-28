fb426541d884835bb5179bb1b65abb15bbae0fa8
Matt McCormick <matt.mccormick@kitware.com>
2021-01-28 09:29:10 -0500

    Headings

--- multi-scale-chunked-compressed/src/App.js
+++ multi-scale-chunked-compressed/src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Heading as="h2" size="lg">Performance impact when loading medical imaging datasets</Heading>
           <VStack spacing={10} divider={<StackDivider borderColor="gray.200" />}>
             <Box maxW="40rem">
-              <Heading as="h3" size="md">Overview</Heading>
+              <Heading as="h3" size="lg">Overview</Heading>
                 <Text>Multi-scale, chunked, medical imaging volumes compressed with <Link color="teal.500" href="https://blosc.org/" target="_blank" rel="noopener noreferrer">next-generation codecs</Link> result in a better user experience on the web due to improved <Link color="teal.500" href="https://web.dev/fid/" target="_blank" rel="noopener noreferrer">interactivity</Link> and <Link color="teal.500" href="https://web.dev/lcp/" target="_blank" rel="noopener noreferrer">loading performance</Link>. These benefits originate from multiple properties of these next-generation file formats:
               </Text>
                 <br/>
@@ -37,6 +37,37 @@ function App() {
               </OrderedList>
                 <br/>
               <Text>This page profiles the performance impact of these properties, comparing <Link color="teal.500" href="https://nifti.nimh.nih.gov/" target="_blank" rel="noopener noreferrer">NIFTI</Link> volumes to <Link color="teal.500" href="https://zarr.readthedocs.io/en/stable/" target="_blank" rel="noopener noreferrer">Zarr</Link> multi-scale pyramids.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg">Test datasets</Heading>
+              <Text>Three medical imaging volumes, varying in size, are tested.</Text>
+              <HStack divider={<StackDivider borderColor="gray.600" />}>
+                <Box>
+                  <Heading as="h5" size="md">Small</Heading>
+                </Box>
+                <Box>
+                  <Heading as="h5" size="md">Medium</Heading>
+                </Box>
+                <Box>
+                  <Heading as="h5" size="md">Large</Heading>
+                </Box>
+              </HStack>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg">Reduced data size</Heading>
+              <Text>Todo.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg">Progressive, low-to-high scale loading</Heading>
+              <Text>Todo.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg">Parallel data fetching and decompression</Heading>
+              <Text>Todo.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg">Data slicing</Heading>
+              <Text>Todo.</Text>
             </Box>
           </VStack>
         </Grid>
